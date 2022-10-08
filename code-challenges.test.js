e10bedb5703b44b9574051284186ca37878a0092
TheRealRion <112355675+TheRealRion@users.noreply.github.com>
2022-10-07 17:07:19 -0700

    just ruby left

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 5: JavaScript Coding Practical Questions with Jest
 
+
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -10,37 +12,132 @@
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
 
+
+//Template  Red --> Green --> Refactor : 
+// describe("funcName", () => {
+//     it("", () => {
+//       expect(funcName(input)).toEqual(output)
+//     })
+//   })
+
+
 // --------------------1) Create a function that takes in a string and returns a coded message. The coded message converts "a" to 4, "e" to 3, "i" to 1, and "o" to 0.
 
 // a) Create a test with expect statements using the variables provided.
 
-const secretCodeWord1 = "Lackadaisical"
-// Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
-// Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
-// Expected output: "3cc3ntr1c"
+describe("codeMe", () => {
+    const secretCodeWord1 = "Lackadaisical"
+    // Expected output: "L4ck4d41s1c4l"
+    const secretCodeWord2 = "Gobbledygook"
+    // Expected output: "G0bbl3dyg00k"
+    const secretCodeWord3 = "Eccentric"
+    // Expected output: "3cc3ntr1c"
+    it("takes in a string and returns a coded message", () => {
+    expect(codeMe(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+    expect(codeMe(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+    expect(codeMe(secretCodeWord3)).toEqual("3cc3ntr1c")
+    })
+})
+
+//Ran $ yarn jest to ensure "red" test
+//Code fail-yay! 
+// ReferenceError: codeMe is not defined
 
 // b) Create the function that makes the test pass.
+//PsudoCode
+  //1 create a function (codeMe)
+  //2 takes in a string (one peramiter-string)
+  //3 change string to array (.split)
+  //4 iterate over array to find letters (a/e/i/o) and changes to numbers (4/3/1/0) -using if statements
+  //5 puses to temp array (..temp)
+  //6 joins array back together to make string (.join)
+  //7 outputs string (return)
+
+
+const codeMe = (string) => {
+    string= string.toLowerCase()
+    arr= string.split("")
+    temp = []   
+    arr.map((letter => {
+        if (letter === 'a') {
+            letter = '4'
+            return temp = [...temp, letter];
+        } 
+        else if (letter === 'e') {
+            letter = '3'
+            return temp = [...temp, letter];
+        } 
+        else if (letter === 'i') {
+            letter = '1'
+            return temp = [...temp, letter]
+        } 
+        else if ( letter === 'o') {
+            letter = '0'
+            return temp = [...temp, letter]
+        }
+        else {
+            return temp = [...temp, letter]
+        }
+    
+}))
+    return   temp.join('').charAt(0).toUpperCase() + temp.join('').slice(1)
+}
+
+//I struggled with remembering the () after methods here, but thankfully , finally, got it to pass! I know there must be some easier refactoring method available here, so looking forward to Wed to see!
+
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
+describe("findMe", () => {
+    const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+    const letterA = "a"
+    // Expected output: ["Mango", "Apricot", "Peach"]
+    const letterE = "e"
+    // Expected output: ["Cherry", "Blueberry", "Peach"]
+    it("takes in an array of words and a single letter and returns an array of all the words containing that particular letter", () => {
+    expect(findMe(fruitArray, letterA)).toEqual(["Mango", "Apricot", "Peach"])
+    expect(findMe(fruitArray, letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+    })
+})
 
-const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
-
-const letterA = "a"
-// Expected output: ["Mango", "Apricot", "Peach"]
-const letterE = "e"
-// Expected output: ["Cherry", "Blueberry", "Peach"]
+//Ran $ yarn jest to ensure "red" test
+//Code fail-yay! 
+// ReferenceError: findMe is not defined
 
 // b) Create the function that makes the test pass.
+//PsudoCode
+  //1 create a function (findMe)
+  //2 takes in two peramiters (arr, str)
+  //3 iterate over each arr index to find letter from str 
+  //4 puses to temp array (..temp)
+  //5 joins array back together to make string (.join)
+  //6 outputs arr w/ arr strings containing str letter
+
+const findMe = (arr, str) => {
+    temp1 = []
+    arr.map((index) => {
+        temp2=[]
+        temp2= index.toLowerCase().split("")
+        if (temp2.includes(str)) {
+            temp2= temp2.join("").charAt(0).toUpperCase() + temp2.join("").slice(1)
+            console.log(temp2)
+            temp1 = [...temp1, temp2]
+        }
+            
+        }
+    )
+    console.log(temp1)
+    return (temp1)
+} 
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
 
-const hand1 = [5, 5, 5, 3, 3]
+
+describe("fullHouse", () => {
+    const hand1 = [5, 5, 5, 3, 3]
 // Expected output: true
 const hand2 = [5, 5, 3, 3, 4]
 // Expected output: false
@@ -48,5 +145,64 @@ const hand3 = [5, 5, 5, 5, 4]
 // Expected output: false
 const hand4 = [7, 2, 7, 2, 7]
 // Expected output: true
+    it(" takes in an array of 5 numbers and determines whether or not the array is a 'full house'", () => {
+    expect(fullHouse(hand1)).toEqual(true)
+    expect(fullHouse(hand2)).toEqual(false)
+    expect(fullHouse(hand3)).toEqual(false)
+    expect(fullHouse(hand4)).toEqual(true)
+    })
+})
+
+//Ran $ yarn jest to ensure "red" test
+//Code fail-yay! 
+// ReferenceError: fullHouse is not defined
 
 // b) Create the function that makes the test pass.
+
+//PsudoCode
+  //1 create a function (fullHouse)
+  //2 takes in one peramiter (arr)
+  //3 sorts arr (.sort) to make finding matches easier
+    //4a if index 0 & index 1 match
+        //5a if index 0 & index 2 match
+            //5a.1 if index 3 & index 4 match
+                //5a.1a return true
+                //5a.1b return false
+        //5b if index 2 & index 3 match
+            //5b.1a return true
+            //5b.1b return false
+        //5c return false
+    //4b return false
+
+    // winArr = [
+//     [1, 1, 1, 2, 2]
+//     [1, 2, 1, 2, 1]
+//     [1, 2, 2, 1, 1]
+//     [1, 2, 1, 1, 2]
+//     [2, 1, 2, 1, 1]
+//     [2, 2, 1, 1, 1] 
+// ]
+ // [0, 1, 2, 3, 4]
+
+    const fullHouse = (arr) => {
+        arr = arr.sort()
+        if (arr[0] === arr[1]) {
+            if (arr[0] === arr[2]) {
+                if (arr[3] == arr[4]) {
+                    return true
+                }
+            } else if (arr[2] === arr[3]) {
+                if (arr[2] ===arr[4]) {
+                    return true
+                } else {
+                    return false 
+                }
+            } else {
+                return false 
+            }
+        } else {
+            return false
+        }
+    }
+
+//Originally, I had thourhgt to try using a winArr like the tic-tac-toe, but couldn't work the logic for it out in my head, so felt like I had a major breakthrough when I had it simply sort the cards, as any human would automatically do anyways! Then it was easy to chain if/else statements to get the answer. I felt INCREDIBLY accomplished after I got this one to pass!!! 
--- interview-questions.md
+++ interview-questions.md
@@ -8,44 +8,51 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is a gem?
 
-Your answer:
+Your answer: A gem is a type of package manager for Ruby. Various gems assist in different ways to make Ruby easier to use. For example, rspec is a Ruby gem used to test Ruby code and ensure it functions correctly. 
 
-Researched answer:
+Researched answer: Ruby gems are package managers for the programming language Ruby that provide easily managable libraries & programs that are easily installed as an add-on to enhance the Ruby user experience during code development. 
 
 2. What does the acronym CRUD stand for?
 
-Your answer:
+Your answer: CRUD is a Ruby acronym that stands for Create, Read, Update, and Delete. It assists in remembering the steps to manage coding in a database. 
 
-Researched answer:
+Researched answer: CRUD (create, read, update, delete) are the four basic operation that help maintain a database of persistent stored data. 
 
 3. What is Ruby on Rails?
 
-Your answer:
+Your answer: Ruby is a programming language and Rails is a programming tool for Ruby that makes it easier to input both frint-end and back-end development in the Ruby language. 
 
-Researched answer:
+Researched answer: Ruby on Rails is a full-stack (front & back end) web-app framework designed to more easily implement usage of the Ruby programming language. 
 
 4. What is a relational database? Are there other kinds of databases?
 
-Your answer:
+Your answer: A relational database uses the relational modle to store and access data in reference to one another. For example, a table. 
 
-Researched answer:
+------------------
+|_Name_|_Address_|
+|_gesf_|_ijw@wal_|
+|_lker_|_wod@hef_|
+|_okjf_|_ped@ioq_|
+------------------
+
+Researched answer: A relational database is a type of database. It stores and provides access to the stored data by relating the data to the other data. For example, a table is a relational database. Other database types include OOB (object-oriented databases), data warehouses, distributed databases, graph databases, open source databases, cloud databases, multi-modal databases, document(JSON) databasres, and self-driving databases. Each have their own specialty and purpose, along with (sometimes) having thier own coding language. 
 
 5. What are primary keys? Why are they important?
 
-Your answer:
+Your answer: Promary keys are unique keys using in a database to reference a row of data. It is important to easily and accurately keep track of data and ensure it is stored and accessed correctly. 
 
-Researched answer:
+Researched answer: The primary key in a database is the column that contains a unique identifier for each table row in a database. A row must have a primary key to CRUD properly. 
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes:
+1. RESTful routes: A RESTful rout maps the HTTP to CRUD actions by translating back and forth. 
 
-2. JSON:
+2. JSON: JavaScript Object Notation is the file that helps organize JS pack managers (like Yarn, Jest, React, etc). 
 
-3. ERB:
+3. ERB: ERB is embedded Ruby, or inputting Ruby into non-Ruby file types (such as HTML). 
 
-4. Params:
+4. Params: Paramater array
 
-5. API:
+5. API: Application Programming Interface, the way computer programs talk to each other. 
